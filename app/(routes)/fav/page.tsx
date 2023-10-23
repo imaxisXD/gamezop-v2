61cc42d85da87f52851a95d1562a7f56acbf66ef
imaxisXD <sunny735084@gmail.com>
2023-10-23 14:14:00 +0530

    fixed: typescript err

--- app/(routes)/fav/page.tsx
+++ app/(routes)/fav/page.tsx
@@ -1,15 +1,11 @@
 import FavGames from "@/components/ui/fav-games";
-import { getData } from "@/utils/fetchdata";
-import { GameData } from "@/utils/types";
 
-export default async function FavPage() {
-    const results: GameData = await getData();
-    const allGames = results.data.games;
 
+export default function FavPage() {
     return (
         <section className='flex flex-col justify-center items-start gap-6'>
             <h1 className='text-white text-3xl'><span className='text-blue-400'>Category:</span> My Favorites</h1>
-            <FavGames allGames={allGames} />
+            <FavGames />
         </section >
     )
 }
